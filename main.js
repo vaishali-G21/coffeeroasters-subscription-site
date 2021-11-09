bc00b7d3538fdbd4476518a0b32e8c5f1dcbb137
DESKTOP-175V7GI\swapn <vaishali.ghorpade@siliconstack.au.com>
2021-11-09 08:49:40 +0530

    mobile home screen

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
+function myFunction(x) {
+  x.classList.toggle("change");
+  const navs = document.querySelectorAll('.navbar-items')
+  navs.forEach(nav => nav.classList.toggle('navbar-ToggleShow'));
 
+}
